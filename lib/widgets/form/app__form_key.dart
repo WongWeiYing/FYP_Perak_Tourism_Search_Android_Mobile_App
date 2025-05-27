68304589714c16541d14824ac9a65138721798ad
WongWeiYing <168903416+WongWeiYing@users.noreply.github.com>
2025-05-27 16:27:46 +0800

    Update app__form_key.dart

--- lib/widgets/form/app__form_key.dart
+++ lib/widgets/form/app__form_key.dart
@@ -11,44 +11,9 @@ class AppFormFieldKey {
 
   static const nameKey = 'name';
   static const businessNameKey = 'businessName';
-
   static const descriptionKey = 'description';
   static const opHoursKey = 'operatingHours';
   static const contactNoKey = 'contactNumber';
-
-  static const documentNumberKey = 'documentNumber';
-  static const dobKey = 'dob';
-  static const addressKey = 'address';
-  static const postcodeKey = 'postcode';
-  static const cityKey = 'city';
-  static const stateKey = 'state';
-  static const countryKey = 'country';
-  static const correspondingAddressKey = 'correspondingAddress';
-  static const correspondingPostcodeKey = 'correspondingPostcode';
-  static const correspondingCityKey = 'correspondingCity';
-  static const correspondingStateKey = 'correspondingState';
-  static const correspondingCountryKey = 'correspondingCountry';
-  static const countryCodeKey = 'countryCode';
-  static const mobileNumberKey = 'mobileNumber';
-  static const nationalityKey = 'nationality';
-  static const genderKey = 'gender';
-  static const residentialStatusKey = 'residentialStatus';
-  static const maritalStatusKey = 'maritalStatus';
-  static const agentReferralCodeKey = 'agentReferralCode';
-  static const documentImageKey = 'documentImage';
-  static const documentFrontImageKey = 'documentFrontImage';
-  static const documentBackImageKey = 'documentBackImage';
-  static const proofDocKey = 'proofDoc';
-  static const supportingDocumentKey = 'supportingDocument';
-  static const reasonKey = 'reason';
-  static const remarkKey = 'remark';
-  static const differentRegisteredAddressKey = 'differentRegisteredAddress';
-  static const sameRegisteredAddressKey = 'sameRegisteredAddress';
-  static const selfContactKey = 'selfContact';
-  static const bankcruptcyDeclarationKey = 'bankcruptcyDeclaration';
-  static const accountDeleteReasonKey = 'accountDeleteReason';
-  static const employmentAddressDetailsFormKey = 'employmentAddressDetailsForm';
-
   static const primaryButtonValidateKey = 'primaryButtonValidate';
 
   //merchant detail
@@ -63,42 +28,5 @@ class AppFormFieldKey {
   static const mContactNoKey = 'mContactNo';
   static const mOpHoursKey = 'mOpHoursNo';
   static const mRegNoKey = 'mRegNo';
-
   static const sharedKey = 'shared';
-}
-
-class AppSettingsKey {
-  AppSettingsKey._();
-
-  static const contactUsUrl = "app.contact_us_url";
-  static const termsAndConditionsUrl = "app.terms_and_conditions_url";
-  static const privacyPolicyUrl = "app.privacy_policy_url";
-
-  static const niuApplicationTenureOptions =
-      "app.niu_application_tenure_options";
-  static const methodOfRedemptionOfWithdrawal =
-      "app.method_of_redemption_of_withdrawal";
-  static const reasonOfWithdrawal = "app.reason_of_withdrawal";
-  static const onboardingAgreement = "app.onboarding_agreement";
-}
-
-class AppConstantsKey {
-  AppConstantsKey._();
-
-  static const gender = 'gender';
-  static const residentialStatus = "residential_status";
-  static const relationshipToSettlor = "relationship";
-  static const martialStatus = "marital_status";
-  static const employmentType = "employment_type";
-  static const industryType = "industry_type";
-  static const relationship = "relationship";
-  static const bankList = "bank_list";
-  static const businessType = "business_type";
-  static const contactUsReason = "contact_us_reason";
-  static const niuTenurePeriod = 'niu_application_tenure';
-  static const niuApplicationType = "niu_application_type";
-  static const sourceIncomeType = "source_of_income";
-  static const annualDeclarationType = "annual_turn_of_declaration";
-  static const redemptionMethodType = "redemption_method_matured";
-  static const corporateEntityType = 'corporate_type_of_entity';
 }
